683ffe15b02aa691a7320b04c1abcc1a92a99d11
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-08 01:16:06 +0300

    bug fix

--- src/app/boards/page.js
+++ src/app/boards/page.js
@@ -134,7 +134,7 @@ export default function Home() {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense>
       <main className="bg-[#f1f7fe] h-screen  antialiased overflow-y-hidden">
         <nav className="bg-white border-b border-gray-200 px-4 py-2.5 fixed left-0 right-0 top-0 z-50">
           <div className="flex flex-wrap justify-between items-center">
