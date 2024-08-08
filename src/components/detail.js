430b1432a8b8d4014dfac82efdc80dd4f602c685
Utku Cengiz <84972875+UTKUC3NGIZ@users.noreply.github.com>
2024-08-08 06:18:42 +0300

    bug fix

--- src/components/detail.js
+++ src/components/detail.js
@@ -599,7 +599,10 @@ export default function Detail({ open, setOpen, detailData, token }) {
                               <form className=" w-full">
                                 <div className="w-full bg-gray-50 rounded-lg border border-gray-200  ">
                                   <div className="py-2 px-4 bg-[#F9FAFB] rounded-t-lg">
-                                    <label for="comment" className="sr-only">
+                                    <label
+                                      htmlFor="comment"
+                                      className="sr-only"
+                                    >
                                       Your comment
                                     </label>
                                     <textarea
@@ -626,8 +629,8 @@ export default function Detail({ open, setOpen, detailData, token }) {
                                           >
                                             <path
                                               stroke="currentColor"
-                                              stroke-linejoin="round"
-                                              stroke-width="2"
+                                              strokeLinejoin="round"
+                                              strokeWidth="2"
                                               d="M1 6v8a5 5 0 1 0 10 0V4.5a3.5 3.5 0 1 0-7 0V13a2 2 0 0 0 4 0V6"
                                             />
                                           </svg>
